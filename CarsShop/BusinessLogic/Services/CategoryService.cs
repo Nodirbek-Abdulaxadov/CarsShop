d420a8c745319f3c4eeb6b9be84cb80e3ccff858
Nodirbek Abdulaxadov <nbkabdulaxadov2000@gmail.com>
2024-02-21 14:38:01 +0500

    Admin template chopildi

--- CarsShop/BusinessLogic/Services/CategoryService.cs
+++ CarsShop/BusinessLogic/Services/CategoryService.cs
@@ -9,7 +9,7 @@ public class CategoryService(IUnitOfWork unitOfWork)
     {
         if (categoryDto == null)
         {
-            throw new ArgumentNullException("CategoryDto was null");
+            throw new CustomException("CategoryDto was null");
         }
 
         if (string.IsNullOrEmpty(categoryDto.Name))
@@ -32,21 +32,71 @@ public class CategoryService(IUnitOfWork unitOfWork)
 
     public void Delete(int id)
     {
-        throw new NotImplementedException();
+        var category = _unitOfWork.Categories.GetById(id);
+
+        if (category == null)
+        {
+            throw new CustomException("Category not found");
+        }
+
+        _unitOfWork.Categories.Delete(category.Id);
     }
 
     public List<CategoryDto> GetAll()
     {
-        throw new NotImplementedException();
+        var categories = _unitOfWork.Categories.GetAll();
+        var list = categories.Select(c => new CategoryDto()
+        {
+            Id = c.Id,
+            Name = c.Name
+        }).ToList();
+
+        return list;
     }
 
     public CategoryDto GetById(int id)
     {
-        throw new NotImplementedException();
+        var category = _unitOfWork.Categories.GetById(id);
+
+        if (category == null)
+        {
+            throw new CustomException("Category not found");
+        }
+
+        var dto = new CategoryDto()
+        {
+            Id = category.Id,
+            Name = category.Name
+        };
+
+        return dto;
     }
 
     public void Update(CategoryDto categoryDto)
     {
-        throw new NotImplementedException();
+        var category = _unitOfWork.Categories.GetById(categoryDto.Id);
+
+        if (category == null)
+        {
+            throw new CustomException("Category not found");
+        }
+
+        if (string.IsNullOrEmpty(categoryDto.Name))
+        {
+            throw new CustomException("Category name is required");
+        }
+
+        if (categoryDto.Name.Length < 3 || categoryDto.Name.Length > 30)
+        {
+            throw new CustomException("Category name must be between 3 and 30 characters");
+        }
+
+        var dto = new Category()
+        {
+            Id = categoryDto.Id,
+            Name = categoryDto.Name
+        };
+
+        _unitOfWork.Categories.Update(dto);
     }
 }
---
+++ CarsShop/Controllers/CategoriesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarsShop.Controllers;
+
+public class CategoriesController(ICategoryService categoryService)
+    : Controller
+{
+    private readonly ICategoryService _categoryService = categoryService;
+
+    public IActionResult Index()
+    {
+        var categories = _categoryService.GetAll();
+        return View(categories);
+    }
+}
